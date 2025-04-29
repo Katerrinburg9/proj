e99229a5384d975a668ca152031413b40b7ea2e1
katerrinburg9 <ekaterina.shipereva@gmail.com>
2025-04-29 12:00:03 +0300

    Доработка логики проектасс

--- src/main/java/com/stepup/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/com/stepup/proj/MainApplication.java
@@ -5,6 +5,6 @@ public class MainApplication {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
-        System.out.println("Hello ");
+        System.out.println("Hello11 ");
     }
 }
